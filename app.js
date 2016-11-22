52c66b2fc29f152af42386ceb08b4c4f97aae593
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2016-11-22 19:40:36 +0800

    asd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.set('port', (process.env.PORT || 3000));
 app.get('/', function(request, response) {
-	response.send("Hello");
+	response.send("Wubu mommy!!!");
 });
 
 app.listen(app.get('port'), function() {
